3be079793f77b244d7babc109f5231670c57e9f4
Vlad White <vladwhiteinbox@outlook.com>
2023-06-23 16:21:19 +0300

    chore: Added more assets in the semantic-release config

--- release.config.js
+++ release.config.js
@@ -7,7 +7,7 @@ module.exports = {
     [
       "@semantic-release/git",
       {
-        "assets": ["package.json"],
+        "assets": ["tsconfig.json", "tailwind.config.js", "package.json", "nodemon.json", "nginx.conf", "LICENSE", "config.example.json", ".prettierrc.json", ".eslintrc.json", ".gitignore","src/**/*"],
         "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
       }
     ]
