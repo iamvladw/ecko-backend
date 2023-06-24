ae1e7ece048a22c7baeee26e2a0f1c2371b4a0ae
Vlad White <vladwhiteinbox@outlook.com>
2023-06-24 05:37:44 +0300

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,33 +1,29 @@
 module.exports = {
     branches: ['development'],
     plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        '@semantic-release/github',
-        [
-            '@semantic-release/git',
-            {
-                assets: [
-                    'tsconfig.json',
-                    'tailwind.config.js',
-                    'package.json',
-                    'nodemon.json',
-                    'nginx.conf',
-                    'LICENSE',
-                    'config.example.json',
-                    '.prettierrc.json',
-                    '.eslintrc.json',
-                    '.gitignore',
-                    'src/**/*'
-                ],
-                message:
-                    'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-            },
-            'semantic-release-version',
-            {
-                packageFile: 'package.json',
-                releaseNotesFile: 'CHANGELOG.md',
-            },
-        ]
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/npm',
+      '@semantic-release/github',
+      [
+        '@semantic-release/git',
+        {
+          assets: [
+            'tsconfig.json',
+            'tailwind.config.js',
+            'package.json',
+            'nodemon.json',
+            'nginx.conf',
+            'LICENSE',
+            'config.example.json',
+            '.prettierrc.json',
+            '.eslintrc.json',
+            '.gitignore',
+            'src/**/*'
+          ],
+          message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+        }
+      ]
     ]
-};
+  };
+  
