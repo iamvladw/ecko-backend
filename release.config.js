bfbd66f8b9ffe058270fb496120186affa7ab44b
Vlad White <vladwhiteinbox@outlook.com>
2023-06-24 05:48:52 +0300

    Update semantic-release

---
+++ release.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    release: {
+        branches: ['development'],
+        plugins: [
+            '@semantic-release/commit-analyzer',
+            '@semantic-release/release-notes-generator',
+            '@semantic-release/github',
+            '@semantic-release/git'
+        ]
+    }
+};
